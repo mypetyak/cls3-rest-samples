c744502bb352cf5679b36281152571e95db6362c
Teja Naraparaju <teja.naraparaju@hickoryground.com>
2014-08-20 17:35:39 -0400

    small cleanup

--- ruby/rest-client/rest_client.rb
+++ ruby/rest-client/rest_client.rb
@@ -5,7 +5,6 @@ require 'launchy'
 client_id = '6fb14207a77abcb000d9e0f3d133902a60ae0bb1924f5b33d9610a198b439938'
 client_secret = '0379d766923739859830cf94d8ad0a78607854f2e9fa945367708a4030cf2cb1'
 
-#response = RestClient.post 'http://localhost:3000/api/v1/oauth/token', {
 response = RestClient.post 'http://localhost:3000/api/v1/oauth/token', {
     grant_type: 'client_credentials',
     client_id: client_id,
@@ -13,7 +12,6 @@ response = RestClient.post 'http://localhost:3000/api/v1/oauth/token', {
 }, :accept => :json
 
 access_token = JSON.parse(response)['access_token']
-#puts RestClient.get 'http://localhost:3000/api/v1/documents', { 'Authorization' => "Bearer #{access_token}" }
 response = RestClient.post(
   'http://localhost:3000/api/v1/documents',
   {:document_type => "A", :acquisition_title => "Title1", :document_number => 1233, :document_date => "2014-06-05"}.to_json,
@@ -38,7 +36,7 @@ response = RestClient.get(
 )
 
 puts "URL to use is #{JSON.parse(response)['launch_url']}"
-#Launchy.open("#{JSON.parse(response)['launch_url']}")
+Launchy.open("#{JSON.parse(response)['launch_url']}")
 
 puts "http://localhost:3000/api/v1/documents/#{document_id}/"
 response = RestClient.get(
